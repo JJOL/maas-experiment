24fc3c88edbd959595693a68e93af04d1b9468c8
JJOL <jjoulk@gmail.com>
2022-05-23 09:57:38 -0500

    Corregir cross validation resultados nan

--- src/modelgen/metrics.py
+++ src/modelgen/metrics.py
@@ -1,22 +1,31 @@
 import numpy as np
 
-def classification_scores(abs_conf_matrix, n_classes):
+def classification_scores(conf_matrix, n_classes):
+    """
+    Returns a dictionary of the results of average F1, average precision and average recall of the categories in the given confussion matrix.
+    """
     metrics = {}
 
-    mat = np.array(abs_conf_matrix)
+    conf_matrix = np.array(conf_matrix)
 
     f1s = []
     precisions = []
     recalls = []
     for i in range(n_classes):
-        TP = mat[i, i]
-        FP = np.sum(mat[:, i]) - TP
-        FN = np.sum(mat[i, :]) - TP
+        TP = conf_matrix[i, i]
+        FP = np.sum(conf_matrix[:, i]) - TP
+        FN = np.sum(conf_matrix[i, :]) - TP
 
-        precision = TP / (TP + FP)
-        recall = TP / (TP + FN)
-
-        F1 = 2 * (precision*recall) / (precision + recall)
+        # Check for cases when divisions by 0 occur to ignore them as results=0
+        if TP == 0:
+            precision = 0
+            recall = 0
+            F1 = 0
+        else:
+            # Metric Formulas: https://blog.exsilio.com/all/accuracy-precision-recall-f1-score-interpretation-of-performance-measures/#:~:text=F1%20score%20%2D%20F1%20Score%20is,have%20an%20uneven%20class%20distribution.
+            precision = TP / (TP + FP)
+            recall = TP / (TP + FN)
+            F1 = 2 * (precision*recall) / (precision + recall)
 
         f1s.append(F1)
         precisions.append(precision)
